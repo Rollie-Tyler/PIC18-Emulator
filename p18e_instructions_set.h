c6b8d19c27bf00055f87f85ea51c81b9d0bfc07a
Rollie-Tyler <alexandre.jp.rault@gmail.com>
2017-06-05 17:32:35 +0200

    Update p18e_instructions_set.h

--- p18e_instructions_set.h
+++ p18e_instructions_set.h
@@ -7,6 +7,22 @@ void p18e_is_addwf (void);
 void p18e_is_addwfc(void);
 void p18e_is_andwf (void);
 void p18e_is_crlf  (void);
+void p18e_is_comf  (void);
+void p18e_is_cpfseq(void);
+void p18e_is_cpfsgt(void);
+void p18e_is_cpfslt(void);
+void p18e_is_decf  (void);
+void p18e_is_decfsz(void);
+void p18e_is_dcfsnz(void);
+void p18e_is_incf  (void);
+void p18e_is_incfsz(void);
+void p18e_is_infsnz(void);
+void p18e_is_iorwf (void);
+void p18e_is_movf  (void);
+void p18e_is_movff (void);
+
+void p18e_is_bcf   (void);
+
 
 typedef void (*pf_instruction_t)(void);
 
